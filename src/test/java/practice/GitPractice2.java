e468514fb84de5da47fb3c3c2348dbfabbdf153b
Nandini G A <Govardhan G A@LAPTOP-M9R4NARV>
2025-11-25 16:50:13 +0530

    second commit

--- src/test/java/practice/GitPractice2.java
+++ src/test/java/practice/GitPractice2.java
@@ -1,11 +1,7 @@
 package practice;
 
-import org.testng.annotations.Test;
-
 public class GitPractice2 {
-	@Test
-	public void m2() {
-		System.out.println("h");
-	}
-
+ public void m1() {
+	 System.out.println("h");
+ }
 }
